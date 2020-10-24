a33c3a4906de56c4dddd01d972d5f2bbe1e5a464
pin0S <pete.g.lynch@gmail.com>
2020-10-24 14:15:15 +1100

    create, new, show actions added to players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -3,4 +3,24 @@ class PlayersController < ApplicationController
     def new
     end
 
+    def create
+        @player = Player.new(params[player_whitelist])
+
+        @player.save
+        redirect_to @player
+    end
+
+    def show
+        @player = Player.find(params[:id])
+    end
+
+
+    private
+    
+    def player_whitelist
+        params.require(:player).permit(:name, :school,
+        :height, :weight, :years, :age, :salary, :free_age,
+        :position, :team)
+    end
+
 end
