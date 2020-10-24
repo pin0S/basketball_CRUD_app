4d79f346ae81b17418b5df2f92e923c2d569e59c
pin0S <pete.g.lynch@gmail.com>
2020-10-24 14:42:20 +1100

    player show, edit, update add to model

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,25 +1,47 @@
 class PlayersController < ApplicationController
 
+    def index
+        @players = Player.all
+    end
+
     def new
+    end
+
+    def edit
+        @player = Player.find(params[:id])
     end
 
     def create
         @player = Player.new(params[player_whitelist])
-
-        @player.save
-        redirect_to @player
+        
+        if @player.save
+            redirect_to @player
+        else
+            render 'new'
+        end
     end
 
+    def update
+        @player = Article.find(params[:id])
+
+        if @player.update(player_whitelist)
+            redirect_to @player
+        else
+            render 'edit'
+        end
+    end
+    
     def show
         @player = Player.find(params[:id])
     end
+
 
 
     private
     
     def player_whitelist
         params.require(:player).permit(:name, :school,
-        :height, :weight, :years, :age, :salary, :free_age,
+        :height, :weight, :years, :age, :salary, :free_agent,
         :position, :team)
     end
 
